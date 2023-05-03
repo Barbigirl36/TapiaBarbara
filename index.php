39691a39f3d602e32eb45a2440828a5e68bb194d
Barbigirl36 <132413471+Barbigirl36@users.noreply.github.com>
2023-05-02 22:48:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -89,7 +89,7 @@ document.getElementById("elnombre").innerHTML="Barbara Tapia";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
 
- document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='eys-kevin.php'>Kevin Aguilar</a></p><p><a href='eys-damaris.php'>Damaris Viallalva</a></p> <a href='eys-nixon.php'>Nixon Ortiz</a></p> ";
+ document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='https://congresoutlvte.org/QuinonezBryan/'>Bryan Quinonez</a></p><a href='https://congresoutlvte.org/TapiaBarbara/'>Barbara Tapia</a></p> <a href='https://congresoutlvte.org/QuinonezIvan/'>Ivan Quinonez</a></p><a href='https://congresoutlvte.org/VasquezElinton/'>Elinton Vasquez</a></p> ";
  </script>
  
 <?php
